8d2bc2e167fd9e96514a5026bd8f6e07b72c1deb
Alex Gonzalez <spokenbird@gmail.com>
2022-04-27 15:17:44 -0700

    WIP Revert to single flow and add HoneyCrisp
    - Page Navigation working with just flow definitions and no page definitions
    
    [#181974151]
    
    Co-authored-by: Colt Borg <cborg@codeforamerica.org>

--- src/main/java/org/codeforamerica/formflowstarter/pages/config/PageWorkflowConfiguration.java
+++ src/main/java/org/codeforamerica/formflowstarter/pages/config/PageWorkflowConfiguration.java
@@ -1,32 +1,14 @@
 package org.codeforamerica.formflowstarter.pages.config;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class PageWorkflowConfiguration {
   private List<NextPage> nextPages;
-//  private Condition skipCondition;
-//  private List<PageDatasource> datasources = new ArrayList<>();
-//  private String groupName;
-//  private String appliesToGroup;
-//  private String dataMissingRedirect;
-//  private String enrichment;
   private String subtleLinkTargetPage;
-
-  public PageWorkflowConfiguration(LinkedHashMap pageWorkflowConfiguration) {
-  }
 
   public Boolean isDirectNavigation() {
     return true;
   }
-
-//  public Subworkflows getSubworkflows(ApplicationData applicationData) {
-//    return applicationData.getSubworkflowsForPageDatasources(datasources);
-//  }
-
-//  public boolean isInAGroup() {
-//    return groupName != null;
-//  }
 }
