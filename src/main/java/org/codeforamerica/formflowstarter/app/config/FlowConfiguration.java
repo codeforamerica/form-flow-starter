7edb00583ac9a92d124ce34d80e7958c39406ff4
Alex Gonzalez <spokenbird@gmail.com>
2022-05-02 13:59:52 -0700

    Update use of Map interface to use of HashMap implementation
    Co-authored-by: Luigi Ray-Montanez <luigi@codeforamerica.org>

--- src/main/java/org/codeforamerica/formflowstarter/app/config/FlowConfiguration.java
+++ src/main/java/org/codeforamerica/formflowstarter/app/config/FlowConfiguration.java
@@ -1,13 +1,14 @@
 package org.codeforamerica.formflowstarter.app.config;
 
-import java.util.Map;
+import java.util.HashMap;
+
 import lombok.Data;
 
 @Data
 public class FlowConfiguration {
 
   private String name;
-  private Map<String, ScreenNavigationConfiguration> flow;
+  private HashMap<String, ScreenNavigationConfiguration> flow;
 
   public ScreenNavigationConfiguration getScreenNavigation(String screenName) {
     return this.flow.get(screenName);
