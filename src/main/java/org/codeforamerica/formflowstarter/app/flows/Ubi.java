d629825f458a1c484ab593220028b2826a6a913b
Colt Borg <cborg@codeforamerica.org>
2022-06-03 17:59:08 -0500

    Add error templates to the remaining inputs

--- src/main/java/org/codeforamerica/formflowstarter/app/flows/Ubi.java
+++ src/main/java/org/codeforamerica/formflowstarter/app/flows/Ubi.java
@@ -2,6 +2,7 @@ package org.codeforamerica.formflowstarter.app.flows;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,7 +10,7 @@ import lombok.Data;
 public class Ubi {
 
   @NotBlank(message = "{validations.make-sure-to-provide-a-first-name}")
-  @Size(min= 2, message = "{validations.test}")
+  @Size(min = 2, message = "{validations.test}")
   String firstName;
 
   @NotBlank
@@ -18,30 +19,39 @@ public class Ubi {
   @Email
   String emailAddress;
 
+  @NotBlank(message = "Fill out phone")
   String phoneNumber;
 
   @NotBlank(message = "Fill out area!")
   String area;
 
   @NotBlank(message = "Fill out a day")
-  @Size(min=1, max=2, message = "Day must not be more than two characters")
+  @Size(min = 1, max = 2, message = "Day must not be more than two characters")
   String dateDay;
 
   @NotBlank(message = "Fill out a month")
-  @Size(min=1, max=2, message = "Month must not be more than two characters")
+  @Size(min = 1, max = 2, message = "Month must not be more than two characters")
   String dateMonth;
 
   @NotBlank(message = "Fill out a year")
-  @Size(min=4, max=4, message = "Year must be exactly four characters")
+  @Size(min = 4, max = 4, message = "Year must be exactly four characters")
   String dateYear;
 
   @NotBlank(message = "Fill out age2")
   String age2;
+
+  @NotEmpty(message = "Fill out gender")
   String gender;
+
+  @NotEmpty(message = "Fill out favorite color")
   String favoriteColor;
+
+  @NotEmpty(message = "Fill out favorite fruit")
   String favoriteFruit;
 
   @NotBlank(message = "Fill out money")
   String money;
+
+  @NotBlank(message = "Fill out ssn")
   String ssn;
 }
