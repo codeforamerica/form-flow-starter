254921edd8ab5e9f58cf3f3fa20d4e57f77f89e2
Alex Gonzalez <spokenbird@gmail.com>
2022-06-21 17:13:20 -0700

    WIP - Working conditional navigation tests

--- src/test/java/org/codeforamerica/formflowstarter/framework/ConditionalNavigationTest.java
+++ src/test/java/org/codeforamerica/formflowstarter/framework/ConditionalNavigationTest.java
@@ -1,12 +1,30 @@
 package org.codeforamerica.formflowstarter.framework;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.codeforamerica.formflowstarter.app.config.ConditionHandler;
 import org.codeforamerica.formflowstarter.testutilities.AbstractMockMvcTest;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(properties = {"flowsConfig=flows-config/test-conditional-navigation.yaml"})
 public class ConditionalNavigationTest extends AbstractMockMvcTest {
+  @MockBean
+  ConditionHandler mockedConditionHandler;
 
-  void shouldGoToPageWhoseConditionIsSatisfied() {
+  @Test
+  void shouldGoToPageWhoseConditionIsSatisfied() throws Exception {
+    when(mockedConditionHandler.handleCondition(anyString())).thenReturn(true);
+    postExpectingNextPageTitle("first", "firstName", "Testy McTesterson", "Second Page");
+    postExpectingNextPageTitle("second", "Other Page");
+  }
 
+  @Test
+  void shouldNotGoToPageWhoseConditionIsNotSatisfied() throws Exception {
+    when(mockedConditionHandler.handleCondition(anyString())).thenReturn(false);
+    postExpectingNextPageTitle("first", "firstName", "Not Testy McTesterson", "Second Page");
+    postExpectingNextPageTitle("second", "Last Page");
   }
 }
