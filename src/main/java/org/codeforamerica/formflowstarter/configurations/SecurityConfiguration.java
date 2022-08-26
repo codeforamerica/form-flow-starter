9c1fd200c12ec97dbfd386538230d871bb1dcfa3
Alex Gonzalez <spokenbird@gmail.com>
2022-08-25 17:25:29 -0700

    Implement Spring Boot Security and enable automatic CSRF token generation

---
+++ src/main/java/org/codeforamerica/formflowstarter/configurations/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package org.codeforamerica.formflowstarter.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity security) throws Exception {
+		security.httpBasic().disable();
+	}
+}
