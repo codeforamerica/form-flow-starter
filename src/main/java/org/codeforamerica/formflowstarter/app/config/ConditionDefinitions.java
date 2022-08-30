226a580fffd547097f21c09ce6060af5e5506abb
Colt Borg <cborg@codeforamerica.org>
2022-08-30 15:58:29 -0500

    Hide add income button when there are incomes for each household member and applicant

--- src/main/java/org/codeforamerica/formflowstarter/app/config/ConditionDefinitions.java
+++ src/main/java/org/codeforamerica/formflowstarter/app/config/ConditionDefinitions.java
@@ -6,39 +6,54 @@ import org.codeforamerica.formflowstarter.app.data.Submission;
 
 public class ConditionDefinitions {
 
-	public static Boolean applicantIsFemale(Submission submission) {
-		var inputData = submission.getInputData();
-		if (inputData.containsKey("gender[]")) {
-			return ((ArrayList<String>) inputData.get("gender[]")).contains("FEMALE");
-		}
-		return false;
-	}
+  public static Boolean applicantIsFemale(Submission submission) {
+    var inputData = submission.getInputData();
+    if (inputData.containsKey("gender[]")) {
+      return ((ArrayList<String>) inputData.get("gender[]")).contains("FEMALE");
+    }
+    return false;
+  }
 
-	public static Boolean hasHousehold(Submission submission) {
-		var inputData = submission.getInputData();
-		if (inputData.containsKey("hasHousehold")) {
-			return inputData.get("hasHousehold").equals("true");
-		}
-		return false;
-	}
+  public static Boolean hasHousehold(Submission submission) {
+    var inputData = submission.getInputData();
+    if (inputData.containsKey("hasHousehold")) {
+      return inputData.get("hasHousehold").equals("true");
+    }
+    return false;
+  }
 
-	public static Boolean incomeSelectedSelf(Submission submission, String uuid) {
-		if (submission.getInputData().containsKey("income")) {
-			// Change logic to suit your needs
-			var incomeArr = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
-			Map<String, Object> personsIncome = incomeArr.stream()
-					.filter(entry -> entry.get("uuid").equals(uuid)).toList().get(0);
-			return personsIncome.get("householdMember")
-					.equals("applicant");
-		}
-		return false;
-	}
+  public static Boolean incomeSelectedSelf(Submission submission, String uuid) {
+    if (submission.getInputData().containsKey("income")) {
+      // Change logic to suit your needs
+      var incomeArr = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
+      Map<String, Object> personsIncome = incomeArr.stream()
+          .filter(entry -> entry.get("uuid").equals(uuid)).toList().get(0);
+      return personsIncome.get("householdMember")
+          .equals("applicant");
+    }
+    return false;
+  }
 
-	public static Boolean householdMemberAlreadyHasIncome(Submission submission, String householdMemberName) {
-		if (submission.getInputData().containsKey("income")) {
-			var incomeArr = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
-			return incomeArr.stream().anyMatch(entry -> entry.get("householdMember").equals(householdMemberName));
-		}
-		return false;
-	}
+  public static Boolean householdMemberAlreadyHasIncome(Submission submission,
+      String householdMemberName) {
+    if (submission.getInputData().containsKey("income")) {
+      var incomeArr = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
+      return incomeArr.stream()
+          .anyMatch(entry -> entry.get("householdMember").equals(householdMemberName));
+    }
+    return false;
+  }
+
+  public static Boolean allHouseholdMembersHaveIncome(Submission submission) {
+    if (submission.getInputData().containsKey("household") && submission.getInputData()
+        .containsKey("income")) {
+      var householdArr = (ArrayList<Map<String, Object>>) submission.getInputData()
+          .get("household");
+      var incomeArr = (ArrayList<Map<String, Object>>) submission.getInputData().get("income");
+
+      // household members + applicant
+      return (householdArr.size() + 1) == incomeArr.size();
+    }
+    return false;
+  }
 }
