4af1a4516c5b1545daaa3ba8971200e02c0f9c11
Colt Borg <cborg@codeforamerica.org>
2022-09-01 12:55:07 -0500

    Add tests for csrf removal methods
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>
    Co-authored-by: Chibuisi Enyia <cenyia@codeforamerica.org>

--- src/test/java/org/codeforamerica/formflowstarter/app/repository/SubmissionRepositoryServiceTest.java
+++ src/test/java/org/codeforamerica/formflowstarter/app/repository/SubmissionRepositoryServiceTest.java
@@ -5,30 +5,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import org.codeforamerica.formflowstarter.app.data.Submission;
-import org.codeforamerica.formflowstarter.app.data.SubmissionRepository;
+import org.codeforamerica.formflowstarter.app.data.SubmissionRepositoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
 @ActiveProfiles("test")
 // TODO: rework to work with h2
 //@Sql(statements = {"ALTER SEQUENCE submissions_id_seq RESTART WITH 12", "TRUNCATE TABLE submissions"})
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
-class SubmissionRepositoryTest {
+class SubmissionRepositoryServiceTest {
 
   @Autowired
-  private SubmissionRepository submissionRepository;
+  private SubmissionRepositoryService submissionRepositoryService;
 
   @Test
   void shouldSaveSubmissionsWithSequentialIds() {
@@ -36,8 +37,8 @@ class SubmissionRepositoryTest {
     Submission secondSubmission = new Submission();
     firstSubmission.setFlow("testFlow");
     secondSubmission.setFlow("testFlow");
-    submissionRepository.save(firstSubmission);
-    submissionRepository.save(secondSubmission);
+    submissionRepositoryService.save(firstSubmission);
+    submissionRepositoryService.save(secondSubmission);
     assertThat(firstSubmission.getId()).isEqualTo(1);
     assertThat(secondSubmission.getId()).isEqualTo(2);
   }
@@ -55,9 +56,10 @@ class SubmissionRepositoryTest {
         .submittedAt(Date.from(timeNow))
         .build();
 
-    submissionRepository.save(submission);
+    submissionRepositoryService.save(submission);
 
-    Optional<Submission> savedSubmissionOptional = submissionRepository.findById(submission.getId());
+    Optional<Submission> savedSubmissionOptional = submissionRepositoryService.findById(
+        submission.getId());
     Submission savedSubmission = null;
     if (savedSubmissionOptional.isPresent()) {
       savedSubmission = savedSubmissionOptional.get();
@@ -69,7 +71,7 @@ class SubmissionRepositoryTest {
   }
 
   @Test
-  void shouldUpdateExistingApplication() {
+  void shouldUpdateExistingSubmission() {
     var inputData = Map.of(
         "testKey", "this is a test value",
         "otherTestKey", List.of("A", "B", "C"));
@@ -79,21 +81,58 @@ class SubmissionRepositoryTest {
         .flow("testFlow")
         .submittedAt(Date.from(timeNow))
         .build();
-    submissionRepository.save(submission);
+    submissionRepositoryService.save(submission);
 
     var newInputData = Map.of(
         "newKey", "this is a new value",
         "otherNewKey", List.of("X", "Y", "Z"));
     submission.setInputData(newInputData);
-    submissionRepository.save(submission);
+    submissionRepositoryService.save(submission);
 
-    Optional<Submission> savedSubmissionOptional = submissionRepository.findById(submission.getId());
+    Optional<Submission> savedSubmissionOptional = submissionRepositoryService.findById(
+        submission.getId());
     Submission savedSubmission = null;
     if (savedSubmissionOptional.isPresent()) {
       savedSubmission = savedSubmissionOptional.get();
     }
     assertThat(savedSubmission.getInputData()).isEqualTo(newInputData);
+  }
 
+  @Test
+  void shouldRemoveCSRFFromInputData() {
+    var inputData = new HashMap<String, Object>();
+    inputData.put("testKey", "this is a test value");
+    inputData.put("otherTestKey", List.of("A", "B", "C"));
+    inputData.put("_csrf", "a1fd9167-9d6d-4298-b9x4-2fc6c75ff3ab");
+    var submission = Submission.builder()
+        .inputData(inputData)
+        .flow("testFlow")
+        .build();
 
+    submissionRepositoryService.removeFlowCSRF(submission);
+
+    assertThat(submission.getInputData().containsKey("_csrf")).isFalse();
+    assertThat(submission.getInputData().containsKey("testKey")).isTrue();
+  }
+
+  @Test
+  void shouldRemoveCSRFFromSubflowInputData() {
+    var inputData = new HashMap<String, Object>();
+    ArrayList<Map<String, Object>> subflowArr = new ArrayList<>();
+    var subflowMap = new HashMap<String, Object>();
+    subflowMap.put("_csrf", "a1fd9167-9d6d-4298-b9x4-2fc6c75ff3ab");
+    subflowMap.put("foo", "bar");
+    subflowArr.add(subflowMap);
+    inputData.put("household", subflowArr);
+    var submission = Submission.builder()
+        .inputData(inputData)
+        .flow("testFlow")
+        .build();
+
+    submissionRepositoryService.removeSubflowCSRF(submission, "household");
+
+    var subflowEntry = (ArrayList<Map<String, Object>>) submission.getInputData().get("household");
+    assertThat(subflowEntry.get(0).containsKey("_csrf")).isFalse();
+    assertThat(subflowEntry.get(0).containsKey("foo")).isTrue();
   }
 }
