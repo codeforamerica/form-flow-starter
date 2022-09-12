e0dd8eb17b32cacb97b62bbd9383ced48cff059f
Colt Borg <cborg@codeforamerica.org>
2022-09-12 16:42:20 -0500

    Start adding javadocs to main class

--- src/main/java/org/codeforamerica/formflowstarter/FormflowstarterApplication.java
+++ src/main/java/org/codeforamerica/formflowstarter/FormflowstarterApplication.java
@@ -3,11 +3,16 @@ package org.codeforamerica.formflowstarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Our main application class that auto-configures the spring boot app
+ */
 @SpringBootApplication
 public class FormflowstarterApplication {
 
+	/**
+	 * @param args  Command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FormflowstarterApplication.class, args);
 	}
-
 }
